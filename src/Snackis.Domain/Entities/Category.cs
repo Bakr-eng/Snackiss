fa0a79e6270603ff29660ecdcc2ba09e848273f8
Bakr <demlyoafrin@gmail.com>
2026-05-19 08:29:56 +0200

    testing

--- src/Snackis.Domain/Entities/Category.cs
+++ src/Snackis.Domain/Entities/Category.cs
@@ -10,7 +10,6 @@ namespace Snackis.Domain.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
-
         public int? ParentCategoryId { get; set; }
         public Category? ParentCategory { get; set; }
         public ICollection<Category> SubCategories { get; set; } = new List<Category>();
--- src/Snackis.Web/Pages/AdminPage.cshtml.cs
+++ src/Snackis.Web/Pages/AdminPage.cshtml.cs
@@ -1,9 +1,11 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Snackis.Web.Pages
 {
-    public class IndexModel : PageModel
+    [Authorize(Policy = "ShouldBeAdmin")]
+    public class AdminPageModel : PageModel
     {
         public void OnGet()
         {
