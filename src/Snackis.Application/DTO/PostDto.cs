5cace241142687cdff247b3b560ffbfffe462c62
Bakr <demlyoafrin@gmail.com>
2026-06-05 09:38:41 +0200

    still working with api

--- src/Snackis.Application/DTO/PostDto.cs
+++ src/Snackis.Application/DTO/PostDto.cs
@@ -3,7 +3,7 @@
     public class PostDto
     {
         public int Id { get; set; }
-        public string Title { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty; // är empty för att undvika null-värden när man går till azure
         public string Content { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; }
         public string AuthorName { get; set; } = string.Empty;
