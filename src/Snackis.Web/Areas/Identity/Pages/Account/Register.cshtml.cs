cda994d7ec2a6f7a4734ccf6604052755c39bda9
Bakr <demlyoafrin@gmail.com>
2026-06-09 09:30:55 +0200

    deleted space lines in SeedData

--- src/Snackis.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ src/Snackis.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -125,7 +125,7 @@ namespace Snackis.Web.Areas.Identity.Pages.Account
 
                 if (result.Succeeded)
                 {
-                    await _userManager.AddToRoleAsync(user, "User"); // Vid registrering blir Rollen User 
+                    await _userManager.AddToRoleAsync(user, "User");
 
                     _logger.LogInformation("User created a new account with password.");
 
